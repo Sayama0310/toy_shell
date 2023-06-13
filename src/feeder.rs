b5239e191656ff05255789ec47ccf4aa269091be
Sayama0310 <montblanc3060@gmail.com>
2023-06-14 07:42:29 +0900

    [update] Changed the prompt to display "ToySh"
    Replaced 😵‍💫 with 😇 since it was a glyph cluster.

--- src/feeder.rs
+++ src/feeder.rs
@@ -8,13 +8,9 @@ impl Feeder {
     pub(crate) fn feed_line(&self, core: &ShellCore) -> Result<String, ()> {
         // Set the prompt:
         // If the previous execution result is 0, set it to 😊
-        // If the previous execution result is non-zero, set it to 😵‍💫
-        let face = if core.pre_status == 0 {
-            "😊"
-        } else {
-            "😵‍💫"
-        };
-        let prompt = format!("{} ", face);
+        // If the previous execution result is non-zero, set it to 😇
+        let face = if core.pre_status == 0 { "😊" } else { "😇" };
+        let prompt = format!("ToySh {} > ", face);
         print!("{}", prompt);
         io::stdout().flush().expect("Failed to flush stdout");
         // Read a line from stdin:
