da2d39c57bfc6739d3f60621f4cdda2c588ada3e
Sayama0310 <montblanc3060@gmail.com>
2023-06-18 17:50:08 +0900

    [update] Added the behavior to prevent ToySh from exiting even when receiving SIGINT (Ctrl+C) while running.
    To exit ToySh, use the `exit` command.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use crate::core::script::Script;
 use crate::core::ShellCore;
 use crate::feeder::Feeder;
+use nix::sys::signal;
+use nix::sys::signal::{SigHandler, Signal};
+use rustyline::error::ReadlineError;
 use rustyline::history::DefaultHistory;
 use std::process;
 
@@ -8,6 +11,7 @@ mod core;
 mod feeder;
 
 fn main() {
+    ignore_signals();
     let mut core = ShellCore::new();
     let mut feeder = Feeder::<DefaultHistory>::new(&core);
     #[allow(clippy::while_let_loop)]
@@ -17,8 +21,13 @@ fn main() {
                 let mut script = Script::parse(&command, &mut core).unwrap();
                 script.exec(&mut core);
             }
+            Err(ReadlineError::Interrupted) => {
+                // If Ctrl-C is pressed, continue the loop.
+                continue;
+            }
             Err(_) => {
                 // TODO: Handle error
+                eprintln!("Error");
                 break;
             }
         }
@@ -26,4 +35,10 @@ fn main() {
     // FIXME: If process is finished by built-in exit command, history is not saved.
     feeder.save_history(&core);
     process::exit(1);
+}
+
+pub fn ignore_signals() {
+    unsafe {
+        signal::signal(Signal::SIGINT, SigHandler::SigIgn).unwrap();
+    }
 }
